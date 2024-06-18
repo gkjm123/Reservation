4a2374ca0d37c769ecd8901b07f69ee229380bc0
nuni <gkje123@naver.com>
2024-06-18 14:16:46 +0900

    첫번째 커밋

--- src/main/java/com/example/reservation/config/SecurityConfig.java
+++ src/main/java/com/example/reservation/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.reservation.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,7 +11,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
